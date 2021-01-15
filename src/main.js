4a5c884180f4392421445e205d0da3b441112b34
Dohu <adc0612@gmail.com>
2021-01-15 17:11:02 +0900

    vue routing applied

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue';
+import App from './App.vue';
+import router from '@/routes/index';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+  // routes/index.js 에서 router로 import해와 등록한다.
+  router,
+}).$mount('#app');
