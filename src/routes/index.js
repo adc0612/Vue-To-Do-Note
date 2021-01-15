be25f79d73fb5ab0735d9082cf797378d2c0be24
Dohu <adc0612@gmail.com>
2021-01-15 18:43:07 +0900

    vue routing setting

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,30 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import LoginPage from '@/views/LoginPage.vue';
-import SignupPage from '@/views/SignupPage.vue';
 Vue.use(VueRouter);
 
 // router instance를 쓸 수 있게 export한다
 export default new VueRouter({
+  // URL의 #을 없애기 위해
+  mode: 'history',
   routes: [
+    // 주소 default로 접속 할 때 바로 login 페이지로 연경
+    {
+      path: '/',
+      redirect: '/login',
+    },
     {
       path: '/login',
-      component: LoginPage,
+      // component에 function으로 import로 들고오면 해당 페이지에 들어갈 때 해당파일을 자원을 들고 올수 있다.
+      component: () => import('@/views/LoginPage.vue'),
     },
     {
       path: '/signup',
-      component: SignupPage,
+      component: () => import('@/views/SignupPage.vue'),
+    },
+    {
+      // 위에 지정해놓은 페이지가 아닌 URL로 연결 되었을 때 보여줄 페이지
+      path: '*',
+      component: () => import('@/views/NotFoundPage.vue'),
     },
   ],
 });
